9fa81c76bc89ee0d193524882cff101380148334
gebrina <gsonofmika@gmail.com>
2023-09-05 16:15:22 +0300

    created registration form

--- app/user/register/page.tsx
+++ app/user/register/page.tsx
@@ -1,9 +1,66 @@
-import React from "react";
+"use client";
+
+import { registerValidation } from "@/app/validation";
+import { useFormik } from "formik";
 
 const Register = () => {
+  const { values, handleChange, handleSubmit, errors } = useFormik({
+    initialValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+      passowrd: "",
+    },
+    validationSchema: registerValidation,
+    onSubmit: () => {},
+  });
   return (
     <section>
-      <form></form>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="fistName"
+            name="firstName"
+            value={values.firstName}
+            onChange={handleChange}
+          />
+          {errors.firstName && <small>{errors.firstName}</small>}
+        </div>
+
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            value={values.lastName}
+            onChange={handleChange}
+          />
+          {errors.lastName && <small>{errors.lastName}</small>}
+        </div>
+
+        <div>
+          <label htmlFor="email">Email Address</label>
+          <input
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+          />
+          {errors.email && <small>{errors.email}</small>}
+        </div>
+
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="passowrd"
+            onChange={handleChange}
+          />
+          {errors.passowrd && <small>{errors.email}</small>}
+        </div>
+      </form>
     </section>
   );
 };
--- app/validation/index.ts
+++ app/validation/index.ts
@@ -1,6 +1,6 @@
 import * as yup from "yup";
 
-export const RegisterValidation = yup.object().shape({
+export const registerValidation = yup.object().shape({
   firstName: yup.string().required().min(3),
   lastName: yup.string().required().min(3),
   email: yup.string().required().email(),
