1d5903f3ddb1c1407fe601530d3810fc00c88d46
gebrina <gsonofmika@gmail.com>
2023-09-05 12:59:55 +0300

    added active stated to nav

--- app/header/index.tsx
+++ app/header/index.tsx
@@ -1,18 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <header className="h-16 bg-black bg-opacity-25 text-slate-300 flex justify-between items-center px-24 ">
       <Image src={"/logo.ico"} width={40} height={40} alt="Quiz logo" />
       <section className="flex gap-10 text-lg">
-        <Link className="hover:text-yellow-500" href={"/"}>
+        <Link
+          className={`hover:text-yellow-500 ${
+            pathname == "/" && "active-link"
+          }`}
+          href={"/"}
+        >
           Home
         </Link>
-        <Link className="hover:text-yellow-500" href={"/quiz"}>
+        <Link
+          className={`hover:text-yellow-500 ${
+            pathname == "/quiz" && "active-link"
+          }`}
+          href={"/quiz"}
+        >
           Quiz
         </Link>
-        <Link className="hover:text-yellow-500" href={"/quiz/1"}>
+        <Link
+          className={`hover:text-yellow-500 ${
+            pathname == "/quiz-cateogy" && "active-link"
+          }`}
+          href={"/quiz/1"}
+        >
           Quiz Category
         </Link>
       </section>
