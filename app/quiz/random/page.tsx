8fe3db45ac1f5754cdd51fb0b7d325532fa23073
gebrina <gsonofmika@gmail.com>
2023-09-10 15:43:55 +0300

    updated styled choices

--- app/quiz/random/page.tsx
+++ app/quiz/random/page.tsx
@@ -1,30 +1,30 @@
 const page = () => {
   return (
-    <section>
-      <section className="grid container mx-auto md:grid-cols-2">
+    <section className="mt-5">
+      <section className="grid  container mx-auto md:grid-cols-2 gap-4">
         <div className="question py-4 text-xl text-slate-300">
-          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+          <p className="my-2 w-max text-3xl">What is CSS and Sass</p>
 
-          <ul className="flex flex-col gap-y-2 mt-2">
-            <li>
+          <ul className="flex flex-col gap-y-4 mt-2">
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl  p-2 rounded-lg  bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
@@ -34,28 +34,28 @@ const page = () => {
         </div>
 
         <div className="question py-4 text-xl text-slate-300">
-          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+          <p className="my-2 w-max text-3xl">What is Angular Js</p>
 
-          <ul className="flex flex-col gap-y-2 mt-2">
-            <li>
+          <ul className="flex flex-col gap-y-4 mt-2">
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl  p-2 rounded-lg  bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
@@ -65,28 +65,28 @@ const page = () => {
         </div>
 
         <div className="question py-4 text-xl text-slate-300">
-          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+          <p className="my-2 w-max text-3xl">What is Node Js</p>
 
-          <ul className="flex flex-col gap-y-2 mt-2">
-            <li>
+          <ul className="flex flex-col gap-y-4 mt-2">
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl  p-2 rounded-lg  bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
@@ -96,28 +96,28 @@ const page = () => {
         </div>
 
         <div className="question py-4 text-xl text-slate-300">
-          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+          <p className="my-2 w-max text-3xl">What is JavaScript</p>
 
-          <ul className="flex flex-col gap-y-2 mt-2">
-            <li>
+          <ul className="flex flex-col gap-y-4 mt-2">
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl  p-2 rounded-lg  bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
@@ -127,28 +127,59 @@ const page = () => {
         </div>
 
         <div className="question py-4 text-xl text-slate-300">
-          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+          <p className="my-2 w-max text-3xl">What is React Js</p>
 
-          <ul className="flex flex-col gap-y-2 mt-2">
-            <li>
+          <ul className="flex flex-col gap-y-4 mt-2">
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl  p-2 rounded-lg  bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
               </p>
             </li>
-            <li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="question py-4 text-xl text-slate-300">
+          <p className="my-2 w-max text-3xl">How to use CSS in JS</p>
+
+          <ul className="flex flex-col gap-y-4 mt-2">
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li className="text-2xl  p-2 rounded-lg  bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li className="text-2xl p-2 rounded-lg border-slate-500 bg-slate-500 bg bg-opacity-25 hover:bg-opacity-10 cursor-pointer">
               <p>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
                 neque!
