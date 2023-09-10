0aaf48e1b8fb2212fd37a4669f2b7e142ec8df53
gebrina <gsonofmika@gmail.com>
2023-09-10 15:26:19 +0300

    upadate random quiz page

--- app/quiz/random/page.tsx
+++ app/quiz/random/page.tsx
@@ -1,5 +1,164 @@
 const page = () => {
-  return <div>page</div>;
+  return (
+    <section>
+      <section className="grid container mx-auto md:grid-cols-2">
+        <div className="question py-4 text-xl text-slate-300">
+          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+
+          <ul className="flex flex-col gap-y-2 mt-2">
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="question py-4 text-xl text-slate-300">
+          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+
+          <ul className="flex flex-col gap-y-2 mt-2">
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="question py-4 text-xl text-slate-300">
+          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+
+          <ul className="flex flex-col gap-y-2 mt-2">
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="question py-4 text-xl text-slate-300">
+          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+
+          <ul className="flex flex-col gap-y-2 mt-2">
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+          </ul>
+        </div>
+
+        <div className="question py-4 text-xl text-slate-300">
+          <p className="border-b-4 border-yellow-500 w-max">What is React Js</p>
+
+          <ul className="flex flex-col gap-y-2 mt-2">
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+            <li>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+                neque!
+              </p>
+            </li>
+          </ul>
+        </div>
+      </section>
+    </section>
+  );
 };
 
 export default page;
