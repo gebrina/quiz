11fd990bf087eec42f0395ee26bff39d9194fc43
gebrina <gsonofmika@gmail.com>
2023-09-17 14:57:16 +0300

    fetched quiz categories

--- app/quiz-categories/page.tsx
+++ app/quiz-categories/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+
 import Link from "next/link";
+import { useQuery } from "@apollo/client";
+import { getQuizCategoriesQuery } from "../graphql";
 
 const page = () => {
+  const { data, error, loading } = useQuery(getQuizCategoriesQuery);
+
+  if (loading)
+    return (
+      <h1 className="text-center my-10 text-3xl text-slate-300"> Loading...</h1>
+    );
+  if (error)
+    return (
+      <h1 className="text-center my-10 text-3xl text-red-500">
+        Error happened...
+      </h1>
+    );
+
   return (
     <section className="container mx-auto py-5">
       <h1
@@ -14,30 +31,15 @@ const page = () => {
       </h1>
       <section className="text-center mt-12">
         <div className="flex flex-wrap justify-center items-center gap-7">
-          <Link
-            href={"/"}
-            className="h-24 w-full md:w-1/3 text-slate-300 text-3xl flex items-center justify-center hover:bg-opacity-10 hover:cursor-pointer bg-yellow-500 bg-opacity-5  "
-          >
-            React
-          </Link>
-          <Link
-            href={"/"}
-            className="h-24 w-full md:w-1/3 text-slate-300 text-3xl flex items-center justify-center hover:bg-opacity-10 hover:cursor-pointer bg-yellow-500 bg-opacity-5  "
-          >
-            Angular
-          </Link>
-          <Link
-            href={"/"}
-            className="h-24 w-full md:w-1/3 text-slate-300 text-3xl flex items-center justify-center hover:bg-opacity-10 hover:cursor-pointer bg-yellow-500 bg-opacity-5  "
-          >
-            Node JS
-          </Link>
-          <Link
-            href={"/"}
-            className="h-24 w-full md:w-1/3 text-slate-300 text-3xl flex items-center justify-center hover:bg-opacity-10 hover:cursor-pointer bg-yellow-500 bg-opacity-5  "
-          >
-            Express JS
-          </Link>
+          {data.findAllQuizCategory.map((category: any) => (
+            <Link
+              key={category.id}
+              href={`/quiz/${category.name}`}
+              className="h-24 w-full md:w-1/3 text-slate-300 text-3xl flex items-center justify-center hover:bg-opacity-10 hover:cursor-pointer bg-yellow-500 bg-opacity-5  "
+            >
+              {category.name}
+            </Link>
+          ))}
         </div>
       </section>
     </section>
