f61861a41f94cedaaf80d2b45b6cba98e52cd20b
gebrina <gsonofmika@gmail.com>
2023-09-17 18:53:10 +0300

    adisplyed quizes

--- app/quiz-categories/[name]/[id]/page.tsx
+++ app/quiz-categories/[name]/[id]/page.tsx
@@ -18,7 +18,27 @@ const Quiz = () => {
         {error.message}
       </h1>
     );
-  return <section className="container mx-auto"></section>;
+  return (
+    <section className="container mx-auto   text-slate-300 my-10">
+      <h1 className="text-center text-3xl  border-b-2 border-yellow-500 font-bold">
+        {data?.findOneQuizCategory?.name}
+      </h1>
+      <div className="my-7 text-xl flex justify-center items-center flex-col">
+        {data?.findOneQuizCategory?.quizzes?.map((quiz: any, index: number) => (
+          <div key={quiz.id} className="mb-4 md:w-1/2">
+            <p>
+              {index + 1}, {quiz.qusetion}
+            </p>
+            {quiz?.answers?.map((choice: any) => (
+              <ul key={choice.id}>
+                <li>{choice.answer}</li>
+              </ul>
+            ))}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Quiz;
