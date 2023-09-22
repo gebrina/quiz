484e8818debf852ff7e2d915f882a0b1a745b7a3
gebrina <gsonofmika@gmail.com>
2023-09-22 17:46:41 +0300

    displayed number of total questions and others

--- app/quiz/random/page.tsx
+++ app/quiz/random/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 import { getQuizCategoriesQuery } from "@/app/graphql";
 import { useQuery } from "@apollo/client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const RandomQuiz = () => {
   const { error, loading, data } = useQuery(getQuizCategoriesQuery);
   const [quizCategory, setQuizCategory] = useState<any>([]);
+  const [selectedChoice, setSelectedChoice] = useState<string>("");
+  const [answerdChoices, setAnswerdChoices] = useState<any>([]);
+  const [numberofCorrectAnswers, setNumberCorrectAnswers] = useState<number>(0);
+  const totalQuestionsRef = useRef<number>();
 
   useEffect(() => {
     let quizCategoryLength = data?.findAllQuizCategory?.length;
     if (quizCategoryLength > 0) {
       let randomIndex = Math.floor(Math.random() * quizCategoryLength);
       const randomQuizCategory = data?.findAllQuizCategory?.[randomIndex];
+      totalQuestionsRef.current = randomQuizCategory.quizzes.length;
       setQuizCategory(randomQuizCategory);
     }
   }, [data]);
@@ -20,13 +25,21 @@ const RandomQuiz = () => {
     return (
       <h1 className="text-3xl text-center text-slate-300 my-10">Loading...</h1>
     );
+
   if (error) return <h1 className="text-3xl text-red-500">{error.message}</h1>;
+
+  const handleChoiceSelection = (quiz: any, selectedChoice: string) => {};
 
   return (
     <section className="text-slate-300 my-10">
-      <h1 className="text-center text-3xl underline decoration-yellow-500">
-        {quizCategory.name}
-      </h1>
+      <div className="flex justify-center items-center text-center text-3xl gap-24">
+        <h1 className=" underline decoration-yellow-500">
+          {quizCategory.name}
+        </h1>
+        <h1 className="bg-yellow-500 text-xl bg-opacity-40 px-2 rounded-sm shadow-md shadow-yellow-200">
+          Answerd Question: {answerdChoices?.length}/{totalQuestionsRef.current}
+        </h1>
+      </div>
       <div className="my-7 text-xl flex justify-center items-center flex-col">
         {quizCategory?.quizzes?.map((quiz: any, index: number) => (
           <div key={quiz.id} className="mb-4 md:w-1/2">
@@ -38,7 +51,10 @@ const RandomQuiz = () => {
                 className="bg-slate-900 bg-opacity-30 mt-2 flex flex-col gap-2"
                 key={choice.id}
               >
-                <li className="p-2 hover:cursor-pointer hover:bg-yellow-300 hover:bg-opacity-25">
+                <li
+                  onClick={() => handleChoiceSelection(quiz, choice)}
+                  className="p-2 hover:cursor-pointer hover:bg-yellow-300 hover:bg-opacity-25"
+                >
                   {choice.answer}
                 </li>
               </ul>
