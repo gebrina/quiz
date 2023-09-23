2ea6f046ba85bfabdec7584c53e79a2ace88d049
gebrina <gsonofmika@gmail.com>
2023-09-24 00:51:27 +0300

    updated quiz page

--- app/quiz-categories/[name]/[id]/page.tsx
+++ app/quiz-categories/[name]/[id]/page.tsx
@@ -1,14 +1,36 @@
 "use client";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { useQuery } from "@apollo/client";
 import { getQuizCategoryById } from "@/app/graphql";
 import { useParams } from "next/navigation";
+
+type Answer = {
+  id: string;
+  answer: string;
+};
+
+type Question = {
+  answers: Answer[];
+  correctAnswer: string;
+  qusetion: string;
+};
 
 const Quiz = () => {
   const { id } = useParams();
   const { loading, error, data } = useQuery(getQuizCategoryById, {
     variables: { categoryId: id },
   });
+  const [activeQuestion, setActiveQuestion] = useState<Question>();
+  const [questionIndex, setQuestionIndex] = useState(0);
+  const totalQuestionsRef = useRef<number>(0);
+
+  useEffect(() => {
+    const quizzes = data?.findOneQuizCategory?.quizzes;
+    if (quizzes?.length > 0) {
+      totalQuestionsRef.current = quizzes?.length;
+      setActiveQuestion(quizzes[questionIndex]);
+    }
+  }, [data, questionIndex]);
 
   if (loading)
     return <h1 className="text-center my-10 text-3xl">Loading...</h1>;
@@ -20,30 +42,41 @@ const Quiz = () => {
       </h1>
     );
 
+  const handleNext = () => {};
+
   return (
-    <section className="container mx-auto   text-slate-300 my-10">
-      <h1 className="text-center text-3xl  border-b-2 border-yellow-500 font-bold">
+    <section className="container mx-auto md:w-1/2  text-slate-300 my-10">
+      <h1 className="text-center text-3xl underline decoration-yellow-600 font-bold">
         {data?.findOneQuizCategory?.name}
       </h1>
-      <div className="my-7 text-xl flex justify-center items-center flex-col">
-        {data?.findOneQuizCategory?.quizzes?.map((quiz: any, index: number) => (
-          <div key={quiz.id} className="mb-4 md:w-1/2">
-            <p className="font-semibold">
-              {index + 1}, {quiz.qusetion}
-            </p>
-            {quiz?.answers?.map((choice: any) => (
-              <ul
-                className="bg-slate-900 bg-opacity-30 mt-2 flex flex-col gap-2"
-                key={choice.id}
-              >
-                <li className="p-2 hover:cursor-pointer hover:bg-yellow-300 hover:bg-opacity-25">
-                  {choice.answer}
-                </li>
-              </ul>
-            ))}
-          </div>
-        ))}
-      </div>
+      <section className="flex items-start flex-col my-10 gap-2">
+        <p className="text-xl">
+          {questionIndex + 1}, {activeQuestion?.qusetion}
+        </p>
+        <ul className="flex flex-col mt-3 gap-2 text-lg">
+          {activeQuestion?.answers.map((choice: Answer) => (
+            <li
+              className={`bg-slate-900 bg-opacity-80 px-2 py-1 hover:cursor-pointer hover:bg-opacity-30 rounded`}
+              key={choice.id}
+            >
+              {choice.answer}
+            </li>
+          ))}
+          <button
+            className="bg-orange-900 py-2
+             rounded-md
+             transitio-all
+             hover:animate-pulse
+             my-6
+             hover:shadow
+             hover:shadow-yellow-500
+             w-max mx-auto px-12 text-xl"
+            onClick={handleNext}
+          >
+            Next Question
+          </button>
+        </ul>
+      </section>
     </section>
   );
 };
