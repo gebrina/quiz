e9afadeda68455c9cbbf178f35ab52e65bcf795b
gebrina <gsonofmika@gmail.com>
2023-09-24 14:43:59 +0300

    added slected choices handler state

--- app/quiz-categories/[name]/[id]/page.tsx
+++ app/quiz-categories/[name]/[id]/page.tsx
@@ -15,11 +15,18 @@ type Question = {
   qusetion: string;
 };
 
+type SelectedChoice = {
+  idx: number;
+  cIdx: number;
+  answer: string;
+};
+
 const Quiz = () => {
   const { id } = useParams();
   const { loading, error, data } = useQuery(getQuizCategoryById, {
     variables: { categoryId: id },
   });
+
   const [activeQuestion, setActiveQuestion] = useState<Question>();
   const [questionIndex, setQuestionIndex] = useState(0);
   const [timer, setTimer] = useState("00:00");
@@ -27,6 +34,7 @@ const Quiz = () => {
   const minutesRef = useRef<string>("0");
   const timerIntervalRef = useRef<any>(null);
   const totalQuestionsRef = useRef<number>(0);
+  const selectedChoicesRef = useRef<SelectedChoice[]>([]);
 
   const addZeroPrefix = (value: string) => {
     return Number(value) < 10 ? "0" + value : value;
@@ -39,7 +47,7 @@ const Quiz = () => {
       if (secondsRefInNumber === 59) {
         let minutesRefInNumber = Number(minutesRef.current);
         minutesRef.current = (minutesRefInNumber + 1).toString();
-        secondsRef.current = "00";
+        secondsRef.current = "0";
       }
       setTimer(
         addZeroPrefix(minutesRef.current) +
@@ -76,11 +84,16 @@ const Quiz = () => {
     }
   };
 
+  const handleChoiceSelection = (idx: number, cIdx: number, answer: string) => {
+    selectedChoicesRef.current.splice(idx, 1, { idx, cIdx, answer });
+  };
+
   return (
     <section className="container mx-auto md:w-1/2  text-slate-300 my-10">
       <h1 className="text-center text-3xl underline decoration-yellow-600 font-bold">
         {data?.findOneQuizCategory?.name} Quiz
       </h1>
+
       <section className="flex items-start flex-col my-10 gap-2">
         <p className="text-xl">
           {questionIndex + 1}, {activeQuestion?.qusetion}
@@ -92,9 +105,16 @@ const Quiz = () => {
           <span className="bg-yellow-500 text-black px-5 ">{timer}</span>
         </p>
         <ul className="flex flex-col mt-3 gap-3 w-full text-lg">
-          {activeQuestion?.answers.map((choice: Answer) => (
+          {activeQuestion?.answers.map((choice: Answer, index: number) => (
             <li
-              className={`bg-slate-600 bg-opacity-50 px-2  py-1 hover:cursor-pointer hover:bg-opacity-30 rounded`}
+              onClick={() =>
+                handleChoiceSelection(questionIndex, index, choice.answer)
+              }
+              className={`${
+                selectedChoicesRef.current[questionIndex]?.cIdx === index
+                  ? "bg-slate-800 cursor-auto"
+                  : "bg-slate-600  hover:cursor-pointer bg-opacity-50 hover:bg-opacity-30"
+              }   px-2 py-1  rounded`}
               key={choice.id}
             >
               {choice.answer}
